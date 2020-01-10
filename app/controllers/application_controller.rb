2eda7183af808bd8123b71fec47d8267259db2bb
aldenweaver <aldenweavercodes@gmail.com>
2020-01-09 16:39:29 -0800

    4:39 stopping point

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < Sinatra::Base
   register Sinatra::Twitter::Bootstrap::Assets
 
   get '/' do
+    @figures = Figure.all
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :"application/index"
   end
 end
